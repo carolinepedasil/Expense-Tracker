df7710cb5249d15606bf4b90b2fa877277bd8121
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-15 22:23:52 -0300

    Controller

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = 5000;
+require('dotenv').config({ path: './config.env' });
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +11,5 @@ app.use(express.json());
 app.use(require('./routes/route'));
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: http://localhost:${port}`);
 });
