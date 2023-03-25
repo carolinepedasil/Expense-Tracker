01d874c06a967b4af82af80b47ebf1a288ad20a6
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-24 21:07:17 -0300

    RTK Mutation

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -6,24 +6,28 @@ export const apiSlice = createApi({
     baseQuery : fetchBaseQuery({ baseUrl: baseURI }),
     endpoints: builder => ({
         getCategories : builder.query({
-            query: () => '/api/categories'
+            query: () => '/api/categories',
+            providesTags: ['categories']
         }),
         getLabels: builder.query({
-            query: () => '/api/labels'
+            query: () => '/api/labels',
+            providesTags: ['transaction']
         }),
         addTransaction: builder.mutation({
             query: (initialTransaction) => ({
                 url: '/api/transaction',
                 method: 'POST',
                 body: initialTransaction
-            })
+            }),
+            invalidatesTags: ['transaction']
         }),
         deleteTransaction: builder.mutation({
             query: recordId => ({
                 url: '/api/transaction',
                 method: 'DELETE',
                 body: recordId
-            })
+            }),
+            invalidatesTags: ['transaction']
         })
     })
 });
